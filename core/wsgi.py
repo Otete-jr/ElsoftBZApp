692720c04444652bb2f116e88ac613f626baf9c4
OtetePro <adhanjao@gmail.com>
2026-04-08 09:35:42 +0300

    Nimeongeza script ya kutengeneza admin kimyakimya

--- core/wsgi.py
+++ core/wsgi.py
@@ -14,3 +14,13 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
+
+# Chini kabisa ya wsgi.py
+from django.contrib.auth import get_user_model
+User = get_user_model()
+try:
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@elsoft.com', 'Weka_Password_Yako')
+        print("Superuser created successfully!")
+except Exception as e:
+    print(f"Error creating superuser: {e}")
